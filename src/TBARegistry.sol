4b2fc72db7a8bc8eb8c9ac7fe6fc479d6f8ffb8a
Schalk Dormehl <schalk.dormehl@gmail.com>
2023-10-27 22:04:50 +0200

    natspec added

--- src/TBARegistry.sol
+++ src/TBARegistry.sol
@@ -3,6 +3,23 @@ pragma solidity ^0.8.18;
 import "openzeppelin/utils/Create2.sol";
 import "./IERC6551Registry.sol";
 
+/**
+ * @title TBARegistry
+ * @dev This contract implements the registry for Token Bound Accounts (TBA) as per the ERC-6551 standard.
+ * It facilitates the creation and management of accounts bound to non-fungible tokens (NFTs),
+ * enabling each NFT to operate as its own smart contract account.
+ * 
+ * The contract leverages the Ethereum Create2 opcode for deploying smart contracts at specified addresses,
+ * allowing for predictable contract addresses and efficient user onboarding.
+ * 
+ * Each created account is registered within the contract, mapping the account address to its corresponding
+ * NFT's contract address and token ID.
+ *
+ * The contract allows for the computation of account addresses prior to their actual creation,
+ * aiding in the planning and management of TBAs.
+ *
+ * @author Logan Brutsche
+ */
 contract TBARegistry is IERC6551Registry {
     error InitializationFailed();
 
@@ -12,6 +29,16 @@ contract TBARegistry is IERC6551Registry {
     }
     mapping (address => TBA) public registeredAccounts;
 
+    /**
+     * @dev Creates a new account with a bound non-fungible token using the provided parameters and returns the created account's address.
+     * @param _implementation Address of the contract implementation.
+     * @param _chainId Chain ID on which the account is created.
+     * @param _tokenContract Address of the token contract.
+     * @param _tokenId ID of the token to be bound to the new account.
+     * @param _salt A value to modify the resulting address.
+     * @param initData Initialization data to be called on the new account.
+     * @return The address of the created account.
+     */
     function createAccount(
         address _implementation,
         uint256 _chainId,
@@ -51,6 +78,15 @@ contract TBARegistry is IERC6551Registry {
         return _account;
     }
 
+    /**
+     * @dev Computes and returns the address of the account with the provided parameters without actually creating the account.
+     * @param _implementation Address of the contract implementation.
+     * @param _chainId Chain ID for which to compute the account address.
+     * @param _tokenContract Address of the token contract.
+     * @param _tokenId ID of the token for which to compute the account address.
+     * @param _salt A value to modify the resulting address.
+     * @return The address of the computed account.
+     */
     function account(
         address _implementation,
         uint256 _chainId,
@@ -65,6 +101,15 @@ contract TBARegistry is IERC6551Registry {
         return Create2.computeAddress(bytes32(_salt), bytecodeHash);
     }
 
+    /**
+     * @dev Generates the creation code for an account with the provided parameters.
+     * @param _implementation Address of the contract implementation.
+     * @param _chainId Chain ID on which the account is created.
+     * @param _tokenContract Address of the token contract.
+     * @param _tokenId ID of the token to be bound to the new account.
+     * @param _salt A value to modify the resulting creation code.
+     * @return The creation code for the account.
+     */
     function _creationCode(
         address _implementation,
         uint256 _chainId,
--- src/TokenGatedAccount.sol
+++ src/TokenGatedAccount.sol
@@ -10,21 +10,38 @@ import "sstore2/utils/Bytecode.sol";
 import "./IERC6551Account.sol";
 import "./IERC6551Executable.sol";
 
+/**
+ * @title TokenGatedAccount
+ * @dev This contract represents a Token Gated Account (TGA) which serves as an interface for interacting with
+ * various token standards and executing operations based on the ownership and authorization of tokens.
+ * The contract implements multiple interfaces to ensure compatibility and extend functionality.
+ *
+ * @author Logan Brutsche
+ */
 contract TokenGatedAccount is IERC165, IERC1271, IERC6551Account, IERC6551Executable, IERC1155Receiver, IERC721Receiver {
     address public bondedAddress;
     address public tokenOwnerAtLastBond;
 
-    // ensures the msg.sender is either:
-    //  * the token owner
-    //  * the bonded account - UNLESS owner() has changed since that bond call
+    /**
+     * @dev Ensures that the message sender is either the token owner or the bonded account, 
+     * unless the owner has changed since the last bond call.
+     */
     modifier onlyAuthorizedMsgSender() {
         require(_isValidSigner(msg.sender), "Unauthorized caller");
         _;
     }
-
+    
+    /**
+     * @dev Emitted when a new address is bonded to this contract.
+     * @param _newBondedAddress The address that was bonded.
+     */
     event NewBondedAddress(address indexed _newBondedAddress);
 
-    // note that we the bonded address can pass this bond on without authorization from owner()
+    /**
+     * @dev Bonds a specified address to this contract.
+     * Note that we the bonded address can pass this bond on without authorization from owner().
+     * @param _addressToBond The address to bond.
+     */
     function bond(address _addressToBond) 
         external
         onlyAuthorizedMsgSender()
@@ -40,6 +57,14 @@ contract TokenGatedAccount is IERC165, IERC1271, IERC6551Account, IERC6551Execut
     receive() external payable {}
     fallback() external payable {}
 
+    /**
+     * @dev Executes a call on another contract on behalf of the token owner or bonded account.
+     * @param _to The contract address to call.
+     * @param _value The amount of ether to send.
+     * @param _data The call data.
+     * @param operation The operation type (only call operations are supported).
+     * @return result The result data of the call or its revert message.
+     */
     function execute(address _to, uint256 _value, bytes calldata _data, uint operation)
         external
         payable
@@ -60,6 +85,12 @@ contract TokenGatedAccount is IERC165, IERC1271, IERC6551Account, IERC6551Execut
         }
     }
 
+    /**
+     * @dev Returns the token details that this contract is associated with.
+     * @return _chainId The chain ID.
+     * @return _tokenContract The address of the token contract.
+     * @return _tokenId The ID of the token.
+     */
     function token()
         external
         view
@@ -77,6 +108,10 @@ contract TokenGatedAccount is IERC165, IERC1271, IERC6551Account, IERC6551Execut
             );
     }
 
+    /**
+     * @dev Returns the owner of the token associated with this contract from the ERC721 contract.
+     * @return The address of the token owner.
+     */
     function owner() public view returns (address) {
         (uint256 chainId, address tokenContract, uint256 tokenId) = this.token();
         if (chainId != block.chainid) return address(0);
@@ -84,11 +119,22 @@ contract TokenGatedAccount is IERC165, IERC1271, IERC6551Account, IERC6551Execut
         return IERC721(tokenContract).ownerOf(tokenId);
     }
 
+    /**
+     * @dev Checks if this contract supports a specified interface.
+     * @param _interfaceId The ID of the interface to check.
+     * @return true if the interface is supported, false otherwise.
+     */
     function supportsInterface(bytes4 _interfaceId) public pure returns (bool) {
         return (_interfaceId == type(IERC165).interfaceId ||
             _interfaceId == type(IERC6551Account).interfaceId);
     }
 
+    /**
+     * @dev Checks if a specified signer is valid for this contract.
+     * @param signer The address of the signer.
+     * @param calldata The calldata (unused).
+     * @return The function selector if the signer is valid, 0 otherwise.
+     */
     function isValidSigner(address signer, bytes calldata) external view returns (bytes4) {
         if (_isValidSigner(signer)) {
             return IERC6551Account.isValidSigner.selector;
@@ -97,10 +143,21 @@ contract TokenGatedAccount is IERC165, IERC1271, IERC6551Account, IERC6551Execut
         return bytes4(0);
     }
 
+    /**
+     * @dev Internal function to check if a specified signer is valid.
+     * @param signer The address of the signer.
+     * @return true if the signer is valid, false otherwise.
+     */
     function _isValidSigner(address signer) internal view returns (bool) {
         return signer == owner() || (signer == bondedAddress && tokenOwnerAtLastBond == owner());
     }
 
+    /**
+     * @dev Checks if a specified signature is valid for a given hash.
+     * @param hash The hash of the data.
+     * @param signature The signature to check.
+     * @return The function selector if the signature is valid, empty bytes otherwise.
+     */
     function isValidSignature(bytes32 hash, bytes memory signature)
         external
         view
@@ -117,6 +174,15 @@ contract TokenGatedAccount is IERC165, IERC1271, IERC6551Account, IERC6551Execut
         return "";
     }
 
+    /**
+     * @dev Handles the receipt of a single ERC1155 token.
+     * @param address The sender address.
+     * @param address The receiver address.
+     * @param uint256 The ID of the token.
+     * @param uint256 The amount of tokens.
+     * @param bytes calldata The additional calldata.
+     * @return The function selector.
+     */
     function onERC1155Received(
         address,
         address,
@@ -127,6 +193,15 @@ contract TokenGatedAccount is IERC165, IERC1271, IERC6551Account, IERC6551Execut
         return IERC1155Receiver.onERC1155Received.selector;
     }
 
+    /**
+     * @dev Handles the receipt of multiple ERC1155 tokens.
+     * @param address The sender address.
+     * @param address The receiver address.
+     * @param uint256[] calldata The IDs of the tokens.
+     * @param uint256[] calldata The amounts of tokens.
+     * @param bytes calldata The additional calldata.
+     * @return The function selector.
+     */
     function onERC1155BatchReceived(
         address,
         address,
@@ -138,6 +213,14 @@ contract TokenGatedAccount is IERC165, IERC1271, IERC6551Account, IERC6551Execut
         return IERC1155Receiver.onERC1155BatchReceived.selector;
     }
 
+    /**
+     * @dev Handles the receipt of a single ERC721 token.
+     * @param address The sender address.
+     * @param address The receiver address.
+     * @param uint256 The ID of the token.
+     * @param bytes calldata The additional calldata.
+     * @return The function selector.
+     */
     function onERC721Received(
         address,
         address,
